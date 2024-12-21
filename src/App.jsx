eb386ac35ec7db0b2ceabf218aa011109a6ad622
Md Majid Naseem <majidnaseem726@gmail.com>
2024-12-22 00:13:32 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Hero from './components/Hero';
+import Naviagtion from './components/Navigation';
+
+const App = () =>{
+  return (
+  <div>
+    <Naviagtion />
+    <Hero />
+  </div>
+
+  );
+};
+
+export default App;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,24 @@
+const Naviagtion = () =>{
+
+    return (
+
+    <nav className="container">
+      <div className="logo">
+        <img src="/images/amazon.png" alt="logo" />
+      </div>
+
+      <ul>
+        <li href="">Menu</li>
+        <li href="">Location</li>
+        <li href="">About</li>
+        <li href="">Contact</li>
+      </ul>
+
+      <button>login</button>
+
+    </nav>
+
+    );
+};
+
+export default Naviagtion;
